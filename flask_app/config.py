8a7f78f66e778b9a0aa4186486702c88a1e98cdb
disbxys <amenwond408@gmail.com>
2024-09-12 07:49:20 -0700

    Move database file to same location as upload folder

--- flask_app/config.py
+++ flask_app/config.py
@@ -5,8 +5,12 @@ class Config:
 
     SECRET_KEY = "super secret key"
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///image_database.db"
+    # Setup internal database directory
+    internal_db_path = os.path.join(os.getcwd(), "db")
+    os.makedirs(internal_db_path, exist_ok=True)
+    
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(internal_db_path, 'image_database.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), "db", "image_files")
-    THUMBNAIL_FOLDER = os.path.join(os.getcwd(), "db", "thumbnails")
+    UPLOAD_FOLDER = os.path.join(internal_db_path, "image_files")
+    THUMBNAIL_FOLDER = os.path.join(internal_db_path, "thumbnails")
